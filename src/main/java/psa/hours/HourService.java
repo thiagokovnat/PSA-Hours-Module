bec3f8fc729878717804d420cdff185960115506
thiagokovnat <tkovnat@fi.uba.ar>
2021-07-26 17:43:12 -0300

    added restrictions to /hour post

--- src/main/java/psa/hours/HourService.java
+++ src/main/java/psa/hours/HourService.java
@@ -1,11 +1,14 @@
 package psa.hours;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.Optional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class HourService {
@@ -13,7 +16,17 @@ public class HourService {
     @Autowired
     private HourRepository hourRepository;
 
-    public Hours createHours(Hours hour){
+    public boolean datesAreEqual(Date dateOne, Date dateTwo){
+        return (dateOne.getDay() == dateTwo.getDay()) && (dateOne.getMonth() == dateTwo.getMonth()) && (dateOne.getYear() == dateTwo.getYear());
+    }
+
+    public Hours createHours(Hours hour) throws ResponseStatusException{
+        Collection<Hours> hours = this.getHours();
+        for(Hours h: hours){
+            if((hour.getResponsibleResourceID() == h.getResponsibleResourceID()) && (hour.getTaskId() == h.getTaskId()) && (datesAreEqual(h.getDate(), hour.getDate()))){
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User has already loaded hours for that task/date");
+            }
+        }
         return hourRepository.save(hour);
     }
 
